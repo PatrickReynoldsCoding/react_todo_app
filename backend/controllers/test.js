514158aad76f04f6073944964b660ec1bd268ea1
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-07-23 17:59:09 +0100

    updated controller

--- backend/controllers/test.js
+++ backend/controllers/test.js
@@ -1,12 +1,19 @@
-// const Todo = require("../models/todo");
+const Todo = require("../models/todo");
 
 exports.getTest = async (req, res) => {
-  res.status(200).json({
-    message: "hello"
+    const {message} = req.body
+
+    try {
+      const newTodo = await Todo.create({message})
+      res.status(200).json(newTodo)
+      } catch (error){
+      res.status(400).json({error: error.message})
+    }
+    // message: "hello"
     // [
     //   { id: 1, name: "clean car", complete: false},
     //   { id: 2, name: "clean cat", complete: false},
     //   { id: 3, name: "feed cat", complete: false},
     // ]
-  });
+    
 }
